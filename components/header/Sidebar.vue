9d9cc7ff01a8dfefc177c155add35d411d0c54ce
Charis <me@chars.me>
2019-07-07 18:03:11 +0700

    wip

--- components/header/Sidebar.vue
+++ components/header/Sidebar.vue
@@ -38,6 +38,10 @@
           <div class="pullover__wrapper">
             <section class="pullover__about">
               <h2>Blog Facade</h2>
+              <p>
+                I think therefore I am, when I doubt my existence therefore I
+                prove I am exist.
+              </p>
             </section>
             <section class="pullover__recent">
               <ul>
@@ -96,10 +100,29 @@ export default {
   }
 }
 .pullover {
-  @apply w-full h-full relative text-center;
+  @apply w-full h-full relative;
   &__container {
-    @apply absolute top-0 right-0 z-40 opacity-100 bg-black text-white w-1/2 min-h-screen;
+    @apply absolute top-0 right-0 z-40 opacity-100 bg-black text-white w-2/3 min-h-screen text-left p-12;
   }
+  &__toggle {
+    @apply h-12 w-12 block cursor-pointer ml-auto mb-8;
+    &--icon {
+      @apply h-12 w-12 stroke-current;
+    }
+  }
+  &__wrapper {
+    @apply flex relative w-full items-start justify-start flex-col;
+  }
+  &__about {
+    @apply text-center w-full p-16;
+    h2 {
+      @apply text-3xl font-medium text-gray-100 font-sans leading-loose tracking-normal;
+    }
+    p {
+      @apply text-2xl font-light text-gray-300 font-serif leading-loose break-words;
+    }
+  }
+
   &__overlay {
     @apply absolute top-0 left-0 z-30 opacity-50 bg-white w-screen h-screen;
   }
