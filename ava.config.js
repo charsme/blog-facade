092d29b99d7542420711daea8b393cedec88fbb9
Charis <me@chars.me>
2019-07-08 11:15:30 +0700

    nightwatch setup

--- ava.config.js
+++ ava.config.js
@@ -1,7 +1,7 @@
 export default {
   compileEnhancements: false,
   extensions: ['ts'],
-  require: ['ts-node/register', './test/helpers/setup.js'],
+  require: ['ts-node/register', './test/_ava_setup.js'],
   sources: ['**/*.{js,vue}'],
   snapshotDir: './test/snapshot'
 }
