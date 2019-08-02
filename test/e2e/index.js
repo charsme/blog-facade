dda5f9466c4cc5b58bda87b335e1fb9b5bb1f904
Charis <me@chars.me>
2019-08-02 16:57:24 +0700

    e2e test

--- test/e2e/index.js
+++ test/e2e/index.js
@@ -1,13 +1,8 @@
 module.exports = {
-  'Demo test Google': function(browser) {
+  'Demo test Google': browser => {
     browser
-      .url('https://www.google.com')
-      .waitForElementVisible('body')
-      .setValue('input[type=text]', 'nightwatch')
-      .waitForElementVisible('input[name=btnK]')
-      .click('input[name=btnK]')
-      .pause(1000)
-      .assert.containsText('#main', 'Night Watch')
+      .url('http://localhost:3000/')
+      .waitForElementVisible('div.site')
       .end()
   }
 }
